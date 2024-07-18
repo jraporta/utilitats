c9895978c96a1c336a38e82302334fdb898794b6
jraporta <jraporta@gmail.com>
2024-07-18 20:43:37 +0200

    implemented Iterator interface

--- Csv.java
+++ Csv.java
@@ -1,14 +1,22 @@
-package n3exercici1;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 
-public class Csv {
+public class Csv implements Iterable<String[]>{
 	
+	private Path path;
 	
+	public Csv (Path path) {
+		this.path = path;
+	}
+	
+	public Path getPath() {
+		return this.path;
+	}
 	
 	public static Collection<String[]> toCollection(Collection<String[]> c, Path path) throws IOException {
 		c.clear();
@@ -18,6 +26,18 @@ public class Csv {
 			}
 		}
 		return c;
+	}
+
+	@Override
+	public Iterator<String[]> iterator() {
+		Iterator<String[]> ite = null;
+		try {
+			ite = Csv.toCollection(new HashSet<String[]>(), this.path).iterator();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ite;
 	}
 	
 	
